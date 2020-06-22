733cfe5171067074165c4d5de20062c88c3eeffd
Yamin <yamin@iMac-de-Yamin.local>
2020-06-22 17:19:55 +0200

    av

--- get_face.py
+++ get_face.py
@@ -3,31 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+from tester import Eigenfaces
+
 #-------------------------------------------------------------------
 
 class	get_img:
-	def		save_img(x, y, w, h, frame, n):
-		if cv2.waitKey(1)&0xFF==ord('s'):
-			img = frame[y+2:y+h-1, x+2:x+w-1]
-			res = cv2.resize(img, dsize=(270, 270), interpolation=cv2.INTER_CUBIC)
-			mpimg.imsave("save_img/img{:d}.png".format(n), res)
-			print("img{:d}.png: [SAVE]".format(n))
+	def		save_img(x, y, w, h, frame, n, eigen):
+		img = frame[y+2:y+h-1, x+2:x+w-1]
+		res = cv2.resize(img, dsize=(270, 270), interpolation=cv2.INTER_CUBIC)
+		res2 = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+#		cv2.imshow('image',res2)
+		cv2.imwrite("save_img/img.png", res2)
+#		mpimg.imsave("save_img/img.png", res)
+		print("img{:d}.png: [SAVE]".format(n))
+		eigen.tester(eigen)
 	
-	def		process(self):
+	def		process(self, eigen):
 		face_cascade2=cv2.CascadeClassifier("./haarcascade/haarcascade_frontalface_alt2.xml")
 		cap=cv2.VideoCapture(0)
 		nb=1
-		while (1):
+		exit = 1
+		while (exit):
 			ret, frame=cap.read()
 			gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 			face=face_cascade2.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
 			for x, y, w, h in face:
 				cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 				cv2.putText(frame, "face: {:d}".format(nb), (x, y+h+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
-				self.save_img(x, y, w, h, frame, nb)
+				if cv2.waitKey(1)&0xFF==ord('s'):
+					self.save_img(x, y, w, h, frame, nb, eigen)
+#					exit = 0
 				nb=nb+1
-#			if cv2.waitKey(1)&0xFF==ord('q'):
-#				break
+			if cv2.waitKey(1)&0xFF==ord('q'):
+				break
 			cv2.imshow('vew', frame)
 		cap.release()
 		cv2.destroyAllWindows()
@@ -36,5 +44,6 @@ class	get_img:
 
 if __name__ == "__main__":
 	img = get_img
-	img.process(img)
+	eigen = Eigenfaces
+	img.process(img, eigen)
 
--- tester.py
+++ tester.py
@@ -9,12 +9,13 @@ from numpy import linalg
 
 #-------------------------------------------------------------------
 
-class 	eigenfaces:
+class 	Eigenfaces:
 	def 	rgb_to_grey(rgb):
-		r, g, b, e = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2], rgb[:,:,3]
-		gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+#		r, g, b, e = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2], rgb[:,:,3]
+#		gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
 #		gray = r + g + b + e
-		return gray
+#		return gray
+		return (rgb)
 
 	def		mean_face(self):
 		i = 0
@@ -37,23 +38,33 @@ class 	eigenfaces:
 		img, moyenne, nb_img = self.mean_face(self)
 		phi = img - moyenne
 		eigen, sigma, v = linalg.svd(phi.transpose(), full_matrices=False)
-		for i in range(eigen.shape[1]):
-			mpimg.imsave("eigenfaces/img{:d}.png".format(i), eigen[:,i].reshape(270,270))
+#		for i in range(eigen.shape[1]):
+#			mpimg.imsave("eigenfaces/img{:d}.png".format(i), eigen[:,i].reshape(270,270))
 		weights = np.dot(phi, eigen)
 		for j in range(nb_img):
 			for i in range(nb_img):
 				new = moyenne + np.dot(weights[j, :i], eigen[:, :i].T)
-				img_id = str(j)+"_"+str(i)
-				mpimg.imsave("new_img/img{:d}.{:d}.png".format(i, j), new.reshape(270,270))
+#				mpimg.imsave("new_img/img{:d}.{:d}.png".format(i, j), new.reshape(270,270))
+		return (eigen, moyenne, weights)
 
-#	def		tester(self)
-#		face = self.rgb_to_grey(imread("img.png", True))
-#		face = np.reshape(face, 270 * 270)
-
+	def		tester(self):
+		eigen, moyenne, weight = self.eigen_face(self)
+		face = self.rgb_to_grey(imread("save_img/img.png", True))
+		face = np.reshape(face, 270 * 270)
+		phi = face - moyenne
+		weight2 = np.dot(phi, eigen)
+		dist = np.min((weight - weight2)**2, axis=1)
+		indice = np.argmin(dist)
+		mindist=np.sqrt(dist[indice])
+		print(mindist)
+		if (mindist < 40):
+			print("OK")
+		else:
+			print("NOT OK")
 
 #-------------------------------------------------------------------
 
 if __name__ == "__main__":
-	eigen = eigenfaces
-	eigen.eigen_face(eigen)
+	eigen = Eigenfaces
+	eigen.tester(eigen)
 
